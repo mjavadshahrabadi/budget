6336aa2fc5031d436dec9bc7a6235bcf4dae57a2
javad shahrabadi <123872286+mjavadshahrabadi@users.noreply.github.com>
2023-12-20 22:27:09 +0330

    change size of signin form container

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -6,8 +6,8 @@ import { SigninForm } from '../components/signin/Signin/SigninForm';
 
 export default function Page(): ReactElement {
     return (
-        <section className="max-w-5xl mx-auto min-h-screen grid place-items-center">
-            <div className="w-full h-auto flex items-center justify-between p-5 bg-white rounded-lg shadow-sm">
+        <section className="max-w-6xl mx-auto min-h-screen grid place-items-center">
+            <div className="w-full h-auto flex items-center justify-between p-8 bg-white rounded-lg shadow-sm">
                 <div className="relative w-1/2 h-[400px]">
                     <Image src="svg/signin/signin.svg" alt="signin-illustration" fill />
                 </div>
